8eaf471c8bd9d69bbc46b0397350019e8b993346
Vicente <vtespinosa@uc.cl>
2019-08-21 08:02:21 -0400

    Herencia

--- 9_class.rb
+++ 9_class.rb
@@ -11,7 +11,23 @@ class Perratzo
   end
 end
 
+class PerratzoTech < Perratzo
+
+  def initialize(piscolits, dj_favorito)
+    super(piscolits)
+    @dj_favorito = dj_favorito
+  end
+
+  def techno
+    puts "Perro vamos a #{@dj_favorito} en la noche?"
+  end
+end
+
 perro = Perratzo.new(3)
-puts perro.piscolits
+perro.piscolits
 # perro.piscolits = 5
 # puts perro.piscolits
+
+zorron = PerratzoTech.new(5, "Green Velvet")
+zorron.piscolits
+zorron.techno
