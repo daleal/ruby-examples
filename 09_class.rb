b82dd4d3dc18e9a4e5dad85934023e741bde74fe
Daniel Leal <dlleal@uc.cl>
2020-03-12 22:18:47 -0300

    Add comments to classes explanations

--- 09_class.rb
+++ 09_class.rb
@@ -1,35 +1,38 @@
 class Perratzo
 
+  ### Attribute accessor for re-running the module using it ###
   # attr_accessor :piscolits
 
-  def initialize(piscolits)
-    @piscolits = piscolits
+  def initialize(piscolits)  # __init__ method in python
+    @piscolits = piscolits   # @attributes are instance attributes
   end
 
-  def piscolits
+  def piscolits  # Define a method named piscolits
     puts "Tengo #{@piscolits} piscolits"
   end
 end
 
-class PerratzoTech < Perratzo
+class PerratzoTech < Perratzo  # Inherit from Perratzo
 
   def initialize(piscolits, dj_favorito)
-    super(piscolits)
+    super(piscolits)  # Call Perratzo's initialize method
     @dj_favorito = dj_favorito
   end
 
-  def techno
+  def techno  # Define a method named techno
     puts "Perro vamos a #{@dj_favorito} en la noche?"
   end
 end
 
+# Create standard Perratzo instance
 perro = Perratzo.new(3)
 perro.piscolits
-# perro.piscolits = 5
-# puts perro.piscolits
 
+### This won't work unless the attribute accessor is uncommented ###
+# perro.piscolits = 5
+# puts perro.piscolits  # This now calls the attribute instead of the method
+
+# Create PerratzoTech instance
 zorron = PerratzoTech.new(5, "Green Velvet")
 zorron.piscolits
 zorron.techno
-
-# Investigar los modules
