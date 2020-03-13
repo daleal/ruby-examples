1ed70a44abb36911866e8ad723bd8a357305e891
Daniel Leal <dlleal@uc.cl>
2020-03-12 21:57:44 -0300

    Add comments

--- 04_conversions.rb
+++ 04_conversions.rb
@@ -1,8 +1,17 @@
-three = '3'
-five = '5'
+# Define arbitrary strings that LOOK like integers
+three = "3"
+five = "5"
+
+# Naive addition (returns the string concatenation)
 naive = three + five
-real = three.to_i + five.to_i
-overkill = three.to_f + five.to_f
+
+# Adds the integers after getting them from the strings
+real = three.to_i + five.to_i  # Returns an integer
+
+# Adds the floats after getting them from the strings
+overkill = three.to_f + five.to_f  # Returns a float
+
+# Log results
 puts naive
 puts real
 puts overkill
