28554f590e83ad628695f6fb753827e433d99a0c
Daniel Leal <dlleal@uc.cl>
2020-03-12 22:24:03 -0300

    Add comments

--- 10_iterate.rb
+++ 10_iterate.rb
@@ -1,17 +1,19 @@
-list = ['Lunes', 'Lunes', 'Miércoles', 'Viernes', 'Viernes', 'Sábado', 'Domingo']
+# Define a VERY realistic weekday list
+week = ['Lunes', 'Lunes', 'Miércoles', 'Viernes', 'Viernes', 'Sábado', 'Domingo']
 
-list.each do |day|
+# Iter over the week and print each day
+week.each do |day|
   puts day
 end
 
-puts "\n"
+puts "\n"  # Separator
 
-(0..6).each do |i| # Incluidos los dos números!
-  puts list[i]
+(0..6).each do |i| # Both numbers (0 and 6) are included!
+  puts week[i]     # Access the i element of week
 end
 
-puts "\n"
+puts "\n"  # Separator
 
-list.each_with_index do |day, index|
+week.each_with_index do |day, index|  # Remember Python's enumerate method?
   puts "#{index}: #{day}"
 end
