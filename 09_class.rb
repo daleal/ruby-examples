3743d2d14660e881e4dfb3476ebe3baf29a36980
Daniel Leal <dlleal@uc.cl>
2020-03-12 22:21:58 -0300

    Add more comments

--- 09_class.rb
+++ 09_class.rb
@@ -1,12 +1,13 @@
 class Perratzo
 
-  ### Attribute accessor for re-running the module using it ###
+  ### Attribute accessor for re-running the module using it uncommented ###
   # attr_accessor :piscolits
 
   def initialize(piscolits)  # __init__ method in python
     @piscolits = piscolits   # @attributes are instance attributes
   end
 
+  ### Comment this method to see how the attribute accessor works ###
   def piscolits  # Define a method named piscolits
     puts "Tengo #{@piscolits} piscolits"
   end
@@ -29,7 +30,9 @@ perro = Perratzo.new(3)
 perro.piscolits
 
 ### This won't work unless the attribute accessor is uncommented ###
-# perro.piscolits = 5
+### and the piscolits method is commented ###
+# perro.piscolits = 5  # If the method still exists, this will work but
+#                      # calling perro.piscolits will call the method
 # puts perro.piscolits  # This now calls the attribute instead of the method
 
 # Create PerratzoTech instance
