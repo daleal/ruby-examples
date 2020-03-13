ab1a20f522ab291b6c6be918dcae3c32e0f562ed
Daniel Leal <dlleal@uc.cl>
2020-03-12 22:53:42 -0300

    File related things upgraded

--- 11_file_handling.rb
+++ 11_file_handling.rb
@@ -1,27 +1,27 @@
-require 'csv'
+require "csv"  # Import csv library
 
-File.open('files/read.txt', 'r').each do |line|
+# Open file and print it line by line
+File.open("files/read.txt", "r").each do |line|
   puts line
 end
 
-# Ver diferencia entre File.open(...) y open(...)
-
-File.open('files/write.txt', 'w') do |f|
-  f.puts 'Mi primera línea'
-  f.puts 'Esto se escribe en otra línea!'
+# Print to file!
+File.open("files/write.txt", "w") do |f|
+  f.puts "Mi primera línea"
+  f.puts "Esto se escribe en otra línea!"
 end
 
-# File.open('files/write.txt', 'w') {|file| file.puts "Otra forma de escribir en archivos"}
+# Igual que en python, si el archivo no existe, lo crea
 
-# Igual que en python, si el archivo no existe lo crea
+# File.open("files/write.txt", "w") {|file| file.puts "Otra forma de escribir en archivos"}
 
-File.open('files/read.txt', 'a') do |f|
-  f.puts 'Hola, soy yo de nuevo.'
+# Append string to a file already written
+File.open("files/write.txt", "a") do |f|
+  f.puts "Hola, soy yo de nuevo."
 end
 
-file = CSV.read('files/read.csv')
-puts file
-puts file[0][0]
-
-# Notar que file es un array de dos dimensiones
-# Investigar CSV converters
+# Open a csv file with the module CSV
+csv_file = CSV.read("files/read.csv")
+puts csv_file
+puts csv_file[0][0]
+# Note that csv_file is a bidimensional array.
